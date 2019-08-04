ebeeaae89343f443f18b62a6df4c51ab149689cc
Nogicrockforge <nogicrockforge@outlook.com>
2019-08-04 16:05:32 +0800

    Added a simple comment

--- advance_reduce.js
+++ advance_reduce.js
@@ -7,6 +7,7 @@ var output = fs.readFileSync('data.txt', 'utf8')
     .split('\n')
     .map(line => line.split(' '))
     .reduce((customers, line) => {
+        // Declare an empty array or use the existing one
         customers[line[0]] = customers[line[0]] || [];
         customers[line[0]].push({
             name: line[1],
